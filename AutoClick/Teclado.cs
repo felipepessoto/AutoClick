ef654eb64746f09f4d60ee2cfbdedc81eb990375
Felipe Fujiy Pessoto <felipepessoto@hotmail.com>
2016-01-03 16:26:41 -0200

    .NET 4.5.2

--- AutoClick/Teclado.cs
+++ AutoClick/Teclado.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Runtime.InteropServices;
 using System.Windows.Forms;
 
@@ -21,18 +22,18 @@ namespace AutoClick
 
         public struct KEYDBINPUT
         {
-            public Int16 wVk;
-            public Int16 wScan;
-            public Int32 dwFlags;
-            public Int32 time;
-            public Int32 dwExtraInfo;
-            public Int32 __filler1;
-            public Int32 __filler2;
+            public short wVk;
+            public short wScan;
+            public int dwFlags;
+            public int time;
+            public int dwExtraInfo;
+            public int __filler1;
+            public int __filler2;
         }
 
         public struct INPUT
         {
-            public Int32 type;
+            public int type;
             public KEYDBINPUT ki;
         }
 
@@ -44,7 +45,7 @@ namespace AutoClick
             INPUT input = new INPUT();
             input.type = INPUT_KEYBOARD;
             input.ki.dwFlags = 0;
-            input.ki.wVk = (Int16)vk;
+            input.ki.wVk = (short)vk;
             SendInput(1, ref input, Marshal.SizeOf(input));
         }
 
@@ -53,7 +54,7 @@ namespace AutoClick
             INPUT input = new INPUT();
             input.type = INPUT_KEYBOARD;
             input.ki.dwFlags = KEYEVENTF_KEYUP;
-            input.ki.wVk = (Int16)vk;
+            input.ki.wVk = (short)vk;
             SendInput(1, ref input, Marshal.SizeOf(input));
         }
 
